8f78b983e0661ac500fb0c9d23483f6cec08a297
camilo-velandia <camilo.velandia@itglobers.com>
2022-06-10 09:10:28 -0500

    add termomther component

--- react/components/ShippingThermometer.tsx
+++ react/components/ShippingThermometer.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
+import { useOrderForm } from 'vtex.order-manager/OrderForm'
+import Termomether from './Termomether'
 
 const ShippingThermometer = () => {
+
+  const {orderForm} = useOrderForm()
+  const shippingValueFree= 100000
+  const orderValue= orderForm?.totalizers[0]?.value/100
+  const [isFree, setIsFree] = useState(false)
+  const [missinValue, setMissinValue] = useState(shippingValueFree)
+  const [width, setWidth] = useState(0)
+
+  useEffect(() => {
+    setIsFree(orderValue > shippingValueFree)
+    setMissinValue(shippingValueFree - orderValue )
+    setWidth(orderValue/shippingValueFree*100)
+  }, [orderForm?.value])
+
+
+
+  console.log('orderForm',orderForm)
+  console.log('width%',width)
+
   return (
-    <div>
-      ShippingThermometer
+    <div className='pa5 tc'>
+      <Termomether width={width} />
+      {
+        isFree ? <p>Tu envio es gratis!</p> : <p>Te falta ${missinValue} para difrutar de envio gratis Prime <strong>de tus productos de mercado</strong></p>
+      }
     </div>
   );
 };
---
+++ react/components/Termomether.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { IconTruck } from '@vtex/admin-ui';
+import styles from './styles.css'
+
+type Props = {
+  width:number
+}
+
+const Termomether = ({width}:Props) => {
+
+
+
+  return (
+    <div>
+      <div className={styles.progressContainer}>
+        <div className={styles.progressbar}>
+          <div
+            className={styles.progressbarFill}
+            style={{
+              width: `${width}%`,
+              backgroundColor: "#ff50f6"
+            }}
+            />
+        </div>
+        <div
+          className={width >= 100 ? styles.iconActive : styles.icon}
+        >
+          <IconTruck />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Termomether;
